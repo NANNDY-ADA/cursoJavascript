347752f2903b5c61dacad57651b2bce71c60c5b2
ada <nanndy.ada@gmail.com>
2022-10-20 09:58:27 -0500

    OPERADORES_1

---
+++ 3.Operadores/index.js
@@ -0,0 +1,34 @@
+//ronysosaya@gmail.com
+
+//operadores ----> manipulan el valor de las vaiables permitiendo realizar cálculos 
+//                 complejos y la toma de decisiones lógicas 
+//TIPOS DE OPERADORES
+
+//ASIGNACIÓN
+//NANNDY-----> Guarda un valor especifico (símbolo de =)
+//RONY ------> El operador de asignacion, destina el valor de su operando derecho a su
+//            operando izquierdo, es decir x=y y asigna el valor de y a x.
+let numero = 13 
+var animal = "gato"
+var objeto = "Libro"
+
+//CONCLUSIÓN ---> El operador de asignación nos sirve para designar valores especificos a
+//                la variable que consignemos.
+
+//COMPARACIÓN
+//NANNDY ----> Comprueba si sus dos operadores son iguales y devuelve un resultado booleano (simbolo ==)
+//RONY ------> El operador de comparación sirve para comparar operandos y devolver un valor lógico
+//             en función a la comparación.
+var num_1 = 3
+3 == num_1
+console.log(num_1)
+
+//CONCLUSIÓN ----> El operador de comparación comprueba la similitud de dos valores.
+
+//ARITMÉTICOS
+//NANNDY ----> Manipulaciones matemáticas sobre le valor de las variables numéricas (+, -, x, /)
+//RONY ------> Los operadores aritmericos toman valores literales o variables como operandos, devolviendo
+//             un solo valor numérico.
+let num_0 = 17
+let num_00 = 19
+console.log("La suma es " num_0+ num_00)
